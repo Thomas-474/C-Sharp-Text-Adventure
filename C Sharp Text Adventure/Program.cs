636f5c75805c144f221ffb063ba561887e3be52f
Thomas-474 <teg2004@icloud.com>
2021-09-13 10:12:16 -0400

    Add project files.

---
+++ C Sharp Text Adventure/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Text_Adventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+
+        /* Stats
+            Characters:
+                - Max HP
+                - Current HP
+                - Max MP
+                - Current MP
+                - Strength
+                - Speed
+                - Intelligence
+
+            Weapons:
+                -
+                -
+                -
+        http://howtomakeanrpg.com/a/how-to-make-an-rpg-stats.html?scrlybrkr
+        */
+    }
+}
