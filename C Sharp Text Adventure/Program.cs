aa6a3520fc47368934137dc407df146a5603d597
Thomas-474 <teg2004@icloud.com>
2021-09-14 10:19:58 -0400

    Stat Concepts

--- C Sharp Text Adventure/Program.cs
+++ C Sharp Text Adventure/Program.cs
@@ -10,26 +10,33 @@ namespace C_Sharp_Text_Adventure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            /* Stats
+                http://howtomakeanrpg.com/a/how-to-make-an-rpg-stats.html?scrlybrkr
+
+                Characters:
+                    - Max HP
+                    - Current HP
+                    - Max MP
+                    - Current MP
+                    - Strength
+                    - Speed
+                    - Intelligence
+
+                Weapons:
+                    - Attack Type
+                    - Attack Rate
+                    - Defense Power
+            */
+
+            // string[] charStats = new string[] {maxHP, currentHP, maxMP, currentMP, strength, speed, intelligence};
+
+            Console.WriteLine("Enter only the number that corresponds with each option to make the selection\n");
+            Console.WriteLine("Choose a Class:\n  1. Knight\n  2. Wizard\n  3. Rogue");
+            Console.ReadKey();
+
 
             Console.ReadKey();
         }
 
-        /* Stats
-            Characters:
-                - Max HP
-                - Current HP
-                - Max MP
-                - Current MP
-                - Strength
-                - Speed
-                - Intelligence
-
-            Weapons:
-                -
-                -
-                -
-        http://howtomakeanrpg.com/a/how-to-make-an-rpg-stats.html?scrlybrkr
-        */
     }
 }
